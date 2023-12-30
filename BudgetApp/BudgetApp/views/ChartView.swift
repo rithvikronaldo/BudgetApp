8102c83af6ff625c5d59826cd1f0024c65ca7370
rithvikronaldo <86509846+rithvikronaldo@users.noreply.github.com>
2023-12-30 21:28:30 +0530

    Add files via upload

---
+++ BudgetApp/BudgetApp/views/ChartView.swift
@@ -0,0 +1,40 @@
+import Foundation
+import SwiftUI
+import Charts
+
+struct ChartView: View {
+
+    let data: [SavingsDataPoint]
+
+    var body: some View {
+        VStack {
+            Chart {
+                ForEach(Array(data.enumerated()), id: \.offset) { index, element in
+                    BarMark(x: .value("month", element.month), y: .value("value", element.value))
+                        .foregroundStyle(index % 2 == 0 ? Color.lightBar : Color.darkBar)
+                }
+            }
+            .chartXAxis {
+                AxisMarks(values: .automatic) { _ in
+                    AxisValueLabel()
+                }
+            }
+            .chartYAxis {
+                AxisMarks(position: .leading, values: .automatic) { value in
+                    AxisValueLabel() {
+                        if let intValue = value.as(Int.self) {
+                            if intValue < 1000 {
+                                Text("\(intValue)")
+                                    .font(.body)
+                            } else {
+                                Text("\(intValue/1000)\(intValue == 0 ? "" : "k")")
+                                    .font(.body)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
